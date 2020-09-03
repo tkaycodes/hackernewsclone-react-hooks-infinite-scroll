7ab2f0e8654c70dc89da2787f2ce09eeec50673d
TABISH KHAN <tabish003@hotmail.com>
2020-09-03 03:51:31 -0700

    add axios, useState and useEffect hooks, api call to get storyids succesful

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getStoryIds } from './services/api'
 
-export const App = () => <p>Hello</p>
+export const App = () => {
+
+    const [storyIds, setStoryIds] = useState([]);
+
+    useEffect(() => {
+       getStoryIds().then(data => setStoryIds(data));
+    }, [])
+
+    return (
+        <p>{JSON.stringify(storyIds)}</p>
+    )
+
+}
 
