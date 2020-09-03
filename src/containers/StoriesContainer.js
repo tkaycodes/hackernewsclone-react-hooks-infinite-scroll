adc61c195964b32013b7003fdd30b324408072b9
TABISH KHAN <tabish003@hotmail.com>
2020-09-03 04:13:10 -0700

    api call to get storyInfo via Id successful

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import { getStoryIds } from '../services/api';
+import { getStoryIds, getStory } from '../services/api';
 
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds] = useState([]);
 
     useEffect(() => {
        getStoryIds().then(data => setStoryIds(data));
+       getStory(24362768).then(data => console.log(data))
     }, []);
 
     return <p>{JSON.stringify(storyIds)}</p>;
